34f258b68019ea9cd7cce1040ad2515317f23da1
Zmago Devetak <zmago_devetak@yahoo.com>
2016-03-29 11:26:29 +0000

    changed VCR settings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,7 +16,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.default_cassette_options = { record: :new_episodes  }
-  c.allow_http_connections_when_no_cassette = true
+  c.allow_http_connections_when_no_cassette = false
 end
 
 ActiveRecord::Migration.maintain_test_schema!
